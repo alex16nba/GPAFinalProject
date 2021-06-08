1c5d0449c5a48068f26a211cd6f6124daef51615
livasco <liviuivasco13@gmail.com>
2021-06-08 20:23:53 +0300

    Created footer

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,14 @@
-console.log('from my js file')
+this.addEventListener("DOMContentLoaded", () =>{
+    const questions = document.querySelectorAll(".question")
+    questions.forEach((question) => question.addEventListener("click", () =>{
+  
+        if(question.parentNode.classList.contains("active")){
+            question.parentNode.classList.toggle("active")
+        }
+    else{
+    questions.forEach(question => question.parentNode.classList.remove("active"))
+    question.parentNode.classList.add("active")
+        }
+
+    }))
+}) 
