0cdc5604f347ced30dc7fd26ddc6c91a90e97829
DuncaLuca <luca1993dunca@gmail.com>
2021-06-08 20:10:28 +0300

    finished log In Sign Up Pages

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,31 @@
-console.log('from my js file')
+
+function validation(){
+		let firstNameElem = document.getElementById('firstName').value
+		if(firstNameElem === ""){
+			document.getElementById("firstNameError").style.display = 'block';
+		}
+		else { document.getElementById("firstNameError").style.display = 'none';
+		}
+		
+		let lastNameElem = document.getElementById('lastName').value
+		if(lastNameElem === ""){
+			document.getElementById("lastNameError").style.display = 'block';
+		}
+		else { document.getElementById("lastNameError").style.display = 'none';
+		}
+		
+		let usernameElem = document.getElementById('userName').value
+		if(usernameElem === ""){
+			document.getElementById("usernameError").style.display = 'block';
+		}
+		else{ document.getElementById("usernameError").style.display = 'none';
+		}
+		
+		let passwordElem = document.getElementById('passWord').value
+		if(passwordElem === ""){
+			document.getElementById("passwordError").style.display = 'block';
+		}
+		else {document.getElementById("passwordError").style.display = 'none';
+		}
+}
+
